05cd17fb244752d077c3bd9f0029c15b7041e0e1
John Puddephatt <john@jdp.org.uk>
2016-05-10 16:36:08 +0100

    remove api_url from config file

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -3,6 +3,6 @@
 ssh $REMOTE_USER@$SERVER_IP <<EOF
    echo $REMOTE_PASSWORD | sudo -S docker stop objective8 || echo 'Failed to stop objective8 container'
    sudo docker rm objective8 || echo 'Failed to remove objective8 container'
-   sudo docker rmi dcent/objective8 || echo 'Failed to remove objective8 image'
+   sudo docker rmi johnpuddephatt/objective8 || echo 'Failed to remove objective8 image'
    sudo docker run -d --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -p 8081:8081 --name objective8 --restart=on-failure johnpuddephatt/objective8
 EOF
